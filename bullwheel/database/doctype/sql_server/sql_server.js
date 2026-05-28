abd0aa9f36b9c3d13554d6d452a6f05a682f26e6
Carter <carterluker13@gmail.com>
2026-05-28 21:53:50 +0000

    Implemented Test Connection button with notifications

--- bullwheel/database/doctype/sql_server/sql_server.js
+++ bullwheel/database/doctype/sql_server/sql_server.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2026, Barrie's Ski and Sports and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("SQL Server", {
-// 	refresh(frm) {
+frappe.ui.form.on('SQL Server', {
+    refresh(frm) {
+        frm.add_custom_button(__('Test Connection'), () => {
+            frappe.show_alert({ message: __('Testing connection...'), indicator: 'blue' });
+
+            frappe.call({
+                method: 'bullwheel.database.doctype.sql_server.sql_server.test_connection',
+                args: { doc: frm.doc },
+            });
+        });
+    }
+});
 
 // 	},
 // });
--- bullwheel/database/doctype/sql_server/sql_server.py
+++ bullwheel/database/doctype/sql_server/sql_server.py
@@ -2,7 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.model.document import Document
+from frappe.utils.password import get_decrypted_password
 
 from bullwheel.database.SQLServer import MSSQLDatabase
 
@@ -12,19 +14,23 @@ class SQLServer(Document):
 
 
 @frappe.whitelist()
-def test_connection(docname: str):
-	document = frappe.get_doc("SQL Server", docname)
-	with MSSQLDatabase(
-		server=document.server_name,
-		username=document.username,
-		password=document.password,
-		database=document.database_name,
-	) as database:
-		if database.test_connection():
-			frappe.msgprint(msg="Connection test succeeded!", title="Success", indicator="green")
-		else:
-			frappe.msgprint(
-				msg="Connection test failed. Please check your database and authentication information and try again.",
-				title="Failure",
-				indicator="red",
-			)
+def test_connection(**kwargs):
+	document = json.loads(kwargs.get('doc'))
+	try:
+		with MSSQLDatabase(
+			server=document.get('server_name'),
+			username=document.get('username'),
+			password=get_decrypted_password("SQL Server", document.get('name'), fieldname="password"),
+			database=document.get('database_name')
+		) as database:
+			if database.test_connection():
+				frappe.msgprint(msg="Connection test succeeded!", title="Success", indicator="green")
+			else:
+				raise ConnectionError
+	except:
+		frappe.msgprint(
+					msg="Please check your database and authentication information and try again.",
+					title="Connection Test Failed",
+					indicator="red",
+				)
+
