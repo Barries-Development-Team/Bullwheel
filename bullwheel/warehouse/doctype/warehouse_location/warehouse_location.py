a7bdc3219d86eb0e6f166970133b3e29635af4f0
Carter <carterluker13@gmail.com>
2026-06-02 11:36:27 -0600

    Implemented Warehouse Location validation.

--- bullwheel/warehouse/doctype/warehouse_location/warehouse_location.py
+++ bullwheel/warehouse/doctype/warehouse_location/warehouse_location.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2026, Barrie's Ski and Sports and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.utils.nestedset import NestedSet
 
 
 class WarehouseLocation(NestedSet):
-	pass
+	def validate(self):
+		self.validate_no_inventory_in_group()
+		self.validate_non_group_has_no_children()
+		self.validate_parent_is_group
+
+	def validate_no_inventory_in_group(self):
+		if self.is_group and self.location_inventory_quantities:
+			frappe.throw("Group locations cannot contain inventory. Please move items to leaf locations or uncheck 'Is Group'")
+
+	def validate_non_group_has_no_children(self):
+		if not self.is_group:
+			child_count = frappe.db.count('Warehouse Location', filters={'parent_location': self.name})
+			if child_count > 0:
+				frappe.throw(f"Cannot uncheck 'Is Group': This location has {child_count} child location(s)")
+
+	def validate_parent_is_group(self):
+		if self.parent_location:
+			parent = frappe.get_doc('Warehouse Location', self.parent_location)
+			if not parent.is_group:
+				frappe.throw(f"Parent location '{self.parent_location}' must be a group location")
