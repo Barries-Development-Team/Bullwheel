5d70a01a0dadb5498700c134cefc54f87c4e2a50
Carter <carterluker13@gmail.com>
2026-07-06 19:48:07 +0000

    More Ascend Product layout updates. Added autonaming to Product Price.

--- bullwheel/ascend/doctype/ascend_product/ascend_product.py
+++ bullwheel/ascend/doctype/ascend_product/ascend_product.py
@@ -25,65 +25,76 @@ class AscendProduct(AbstractVirtualDocType):
 	TABLE_NAME = "Products"
 
 	SCHEMA_CONFIG = {
-        'name': 'Products.[Store UPC]',
-        'id': 'Products.ID',
-        'category': 'cat.Topic',
-        'description': 'Products.Description',
-        'price': 'Products.Price',
-        'estimated_cost': 'Products.EstCost',
-        'quantity': 'Products.Quantity',
-        'reorder_level': 'Products.ReorderLevel',
-        'maximum': 'Products.Maximum',
-        'commission': 'Products.Commission',
-        'upc': 'Products.UPC',
-        'mfgr_part_no': 'Products.MfgrPartNo',
-        'reconciled': 'Products.Reconciled',
-        'store_sku': 'Products.[Store UPC]',
-        'keyword': 'Products.Keyword',
-        'location': 'Products.Location',
-        'brand': 'Products.Brand',
-        'color': 'Products.Color',
-        'size': 'Products.Size',
-        'other': 'Products.Other',
-        'division': 'Products.Division',
-        'e_commerce': 'Products.eCommerce',
-        'min2': 'Products.Min2',
-        'max2': 'Products.Max2',
-        'no_label': 'Products.NoLabel',
-        'non_inventory': 'Products.NonInventory',
-        'appt_length': 'Products.ApptLength',
-        'creator_id': 'Products.CreatorID',
-        'modifier_id': 'Products.ModifierID',
-        'date_created': 'Products.DateCreated',
-        'date_modified': 'Products.DateModified',
-        'hide': 'Products.Hide',
-        'loc_from_id': 'Products.LocFromID',
-        'dol_com': 'Products.DolCom',
-        'average_cost': 'Products.AvgCost',
-        'comments': 'Products.Comments',
-        'date_qty_chng': 'Products.DateQtyChng',
-        'print_labels_by_division': 'Products.PrintLabelsByDivision',
-        'row_version': 'Products.Row_Version',
-        'date_reconciled': 'Products.DateReconciled',
-        'modifier_location_id': 'Products.ModifierLocationID',
-        'last_cost': 'Products.LastCost',
-        'concurrency_token': 'Products.ConcurrencyToken',
-        'has_pending_delta': 'Products.HasPendingDelta',
-        'style_name': 'Products.StyleName',
-        'style_number': 'Products.StyleNumber',
-        'season': 'Products.Season',
-        'year': 'Products.Year',
-        'gender': 'Products.Gender'
-}
+		'name': 'Products.[Store UPC]',
+		'id': 'Products.ID',
+		'category': 'cat.Topic',
+		'description': 'Products.Description',
+		'price': 'Products.Price',
+		'estimated_cost': 'Products.EstCost',
+		'quantity': 'Products.Quantity',
+		'reorder_level': 'Products.ReorderLevel',
+		'maximum': 'Products.Maximum',
+		'commission': 'Products.Commission',
+		'upc': 'Products.UPC',
+		'mfgr_part_no': 'Products.MfgrPartNo',
+		'reconciled': 'Products.Reconciled',
+		'store_sku': 'Products.[Store UPC]',
+		'keyword': 'Products.Keyword',
+		'location': 'Products.Location',
+		'brand': 'Products.Brand',
+		'color': 'Products.Color',
+		'size': 'Products.Size',
+		'other': 'Products.Other',
+		'division': 'Products.Division',
+		'e_commerce': 'Products.eCommerce',
+		'min2': 'Products.Min2',
+		'max2': 'Products.Max2',
+		'no_label': 'Products.NoLabel',
+		'non_inventory': 'Products.NonInventory',
+		'appt_length': 'Products.ApptLength',
+		'creator_id': 'Products.CreatorID',
+		'modifier_id': 'Products.ModifierID',
+		'date_created': 'Products.DateCreated',
+		'date_modified': 'Products.DateModified',
+		'hide': 'Products.Hide',
+		'loc_from_id': 'Products.LocFromID',
+		'dol_com': 'Products.DolCom',
+		'average_cost': 'Products.AvgCost',
+		'comments': 'Products.Comments',
+		'date_qty_chng': 'Products.DateQtyChng',
+		'print_labels_by_division': 'Products.PrintLabelsByDivision',
+		'row_version': 'Products.Row_Version',
+		'date_reconciled': 'Products.DateReconciled',
+		'modifier_location_id': 'Products.ModifierLocationID',
+		'last_cost': 'Products.LastCost',
+		'concurrency_token': 'Products.ConcurrencyToken',
+		'has_pending_delta': 'Products.HasPendingDelta',
+		'style_name': 'Products.StyleName',
+		'style_number': 'Products.StyleNumber',
+		'season': 'Products.Season',
+		'year': 'Products.Year',
+		'gender': 'Products.Gender'
+	}
 
 	JOIN_CONFIG = [
-    {
-        "join":  "LEFT JOIN",                          # JOIN type
-        "table": "Categories",                         # Table to join
-        "alias": "cat",                                # Optional alias
-        "on":    "Products.TopicID = cat.ID",          # Full ON condition
-    }
-]
+		{
+			"join":  "LEFT JOIN",                          # JOIN type
+			"table": "Categories",                         # Table to join
+			"alias": "cat",                                # Optional alias
+			"on":    "Products.TopicID = cat.ID",          # Full ON condition
+		}
+	]
+
+	# Swap and Online price display fields are virtual and not stored in the Ascend database,
+	# so we need to fetch them from the Product Price DocType.
+
+	@property
+	def swap_price(self):
+		return frappe.db.get_value('Product Price', f'PRICE-SWAP-{self.name}', 'price')
+
+	@property
+	def online_price(self):
+		return frappe.db.get_value('Product Price', f'PRICE-ONLINE-{self.name}', 'price')
 
 
 @frappe.whitelist()
--- bullwheel/ascend/doctype/product_price/product_price.py
+++ bullwheel/ascend/doctype/product_price/product_price.py
@@ -1,11 +1,26 @@
 # Copyright (c) 2026, Barrie's Ski and Sports and contributors
 # For license information, please see license.txt
 
+from sys import prefix
+
 import frappe
 from frappe.model.document import Document
 
 
 class ProductPrice(Document):
+
+	def autoname(self):
+		"""Set the document name to a unique combination of pricing type and product."""
+		match self.pricing_type:
+			case "Ski Swap Price":
+				type = "SWAP"
+			case "Online Listing Price":
+				type = "ONLINE"
+			case _:
+				raise ValueError(f"Unknown pricing type: {self.pricing_type}. Ensure that the pricing type is one of the expected values: 'Ski Swap Price' or 'Online Listing Price'.")
+
+		self.name = f"PRICE-{type}-{self.product}"
+
 	def validate(self):
 		duplicate_name = frappe.db.exists(
 			"Product Price",
