c7347450f69412783fbe3d6e3127b0d07a19a0af
carter <carterluker13@gmail.com>
2026-07-24 17:25:22 +0000

    Pricing rule validation.

--- bullwheel/ascend/doctype/product_pricing_rule/product_pricing_rule.py
+++ bullwheel/ascend/doctype/product_pricing_rule/product_pricing_rule.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2026, Barrie's Ski and Sports and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ProductPricingRule(Document):
-	pass
+	def validate(self):
+		if self.swap_percentage >= 1.00 or self.online_percentage >= 1.00:
+			frappe.throw("Price percentage cannot be 100% (1.00) or greater.")
