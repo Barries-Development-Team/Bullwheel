eafde023b42087f44375125c94c9a7543baff136
Carter <carterluker13@gmail.com>
2026-09-02 20:04:22 +0000

    Added Swap Tag Batch Doctypes.

---
+++ bullwheel/label_printing/doctype/swap_tag_batch/swap_tag_batch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Barrie's Ski and Sports and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Swap Tag Batch", {
+// 	refresh(frm) {
+
+// 	},
+// });
