21797a4761cd6a3a8901c2243fb402cc344c52d8
Carter <carterluker13@gmail.com>
2026-09-16 17:10:29 +0000

    Swap Tag Batch price display. Virtual Field documentation.

--- bullwheel/label_printing/doctype/swap_tag_batch/swap_tag_batch.py
+++ bullwheel/label_printing/doctype/swap_tag_batch/swap_tag_batch.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2026, Barrie's Ski and Sports and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SwapTagBatch(Document):
-	pass
+	def before_save(self):
+		for row in self.swap_tag_items:
+			row.swap_price = frappe.get_value('Product Price', f'PRICE-SWAP-{row.product}', 'price')
--- bullwheel/label_printing/doctype/swap_tag_batch_item/swap_tag_batch_item.py
+++ bullwheel/label_printing/doctype/swap_tag_batch_item/swap_tag_batch_item.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Barrie's Ski and Sports and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 from bullwheel.ascend.doctype.ascend_product.ascend_product import AscendProduct
@@ -9,6 +9,30 @@ from bullwheel.ascend.doctype.ascend_product.ascend_product import AscendProduct
 
 class SwapTagBatchItem(Document):
 	
+	def _ascend_fields(self):
+		if not hasattr(self, "_ascend_field_cache"):
+			self._ascend_field_cache = (
+				AscendProduct.get_bulk_short_cached_values([self.product], ["description", "estimated_cost", "average_cost", "price"]).get(self.product)
+				if self.product else None
+			)
+		return self._ascend_field_cache
+	
 	@property
 	def description(self):
-		return AscendProduct.get_cached_value(self.product, "description")
+		fields = self._ascend_fields()
+		return fields.get("description") if fields else None
+
+	@property
+	def estimated_cost(self):
+		fields = self._ascend_fields()
+		return fields.get("estimated_cost") if fields else None
+
+	@property
+	def average_cost(self):
+		fields = self._ascend_fields()
+		return fields.get("average_cost") if fields else None
+
+	@property
+	def msrp(self):
+		fields = self._ascend_fields()
+		return fields.get("price") if fields else None
